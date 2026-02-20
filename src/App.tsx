3f58eab5e97d71035ec88bc84abeb12682bde943
Jose Miguel Navarro <jmn8718@gmail.com>
2026-02-20 09:28:24 +0900

    refactor: replace BrowserRouter with HashRouter for routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import BottomNav from './components/BottomNav';
 import Home from './pages/Home';
 import Flights from './pages/Flights';
